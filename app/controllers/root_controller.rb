086ccb724101cff63d5e17f91e9ad0d36e81a1fa
Roberto Dip <dip.jesusr@gmail.com>
2014-03-12 22:22:55 -0300

    LinkedIn integration: added basic functions to get an users profile

--- app/controllers/root_controller.rb
+++ app/controllers/root_controller.rb
@@ -1,4 +1,6 @@
 module Vita::Application
+  require "linkedin"
+
   class RootController < ApplicationController
     set prefix: '/'
     set views_prefix: '/root'
@@ -7,6 +9,15 @@ module Vita::Application
 
     get '/' do
       erb :index
+    end
+
+  private
+    def profile
+      return @profile unless @profile.nil?
+
+      client = LinkedIn::Client.new(ENV['CONSUMER_KEY'],ENV['CONSUMER_SECRET'])
+      client.authorize_from_access(ENV['CLIENT_TOKEN'], ENV['CLIENT_ACCESS'])
+      @profile = client.profile
     end
   end
 end
